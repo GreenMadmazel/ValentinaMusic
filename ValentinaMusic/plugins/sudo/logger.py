2edb5192c32dd3c7a5f19d7ae462d6f3853db12c
ValentineXHashTag <166988254+ValentineXHashTag@users.noreply.github.com>
2024-04-18 16:03:42 +0530

    Update logger.py

--- ValentinaMusic/plugins/sudo/logger.py
+++ ValentinaMusic/plugins/sudo/logger.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.database import add_off, add_on
-from AnonXMusic.utils.decorators.language import language
+from ValentinaMusic import app
+from ValentinaMusic.misc import SUDOERS
+from ValentinaMusic.utils.database import add_off, add_on
+from ValentinaMusic.utils.decorators.language import language
 
 
 @app.on_message(filters.command(["logger"]) & SUDOERS)
