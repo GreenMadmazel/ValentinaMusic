b52a7b9fdb9638f742537aa37b9f7da13defd014
ValentineXHashTag <166988254+ValentineXHashTag@users.noreply.github.com>
2024-04-18 14:45:47 +0530

    Update auth.py

--- ValentinaMusic/plugins/admins/auth.py
+++ ValentinaMusic/plugins/admins/auth.py
@@ -1,16 +1,16 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.utils import extract_user, int_to_alpha
-from AnonXMusic.utils.database import (
+from ValentinaMusic import app
+from ValentinaMusic.utils import extract_user, int_to_alpha
+from ValentinaMusic.utils.database import (
     delete_authuser,
     get_authuser,
     get_authuser_names,
     save_authuser,
 )
-from AnonXMusic.utils.decorators import AdminActual, language
-from AnonXMusic.utils.inline import close_markup
+from ValentinaMusic.utils.decorators import AdminActual, language
+from ValentinaMusic.utils.inline import close_markup
 from config import BANNED_USERS, adminlist
 
 
