b92b8c56003d9d6a01c3319a4f534eeb44139af8
ValentineXHashTag <166988254+ValentineXHashTag@users.noreply.github.com>
2024-04-18 16:14:14 +0530

    Update stats.py

--- ValentinaMusic/plugins/tools/stats.py
+++ ValentinaMusic/plugins/tools/stats.py
@@ -9,13 +9,13 @@ from pyrogram.types import InputMediaPhoto, Message
 from pytgcalls.__version__ import __version__ as pytgver
 
 import config
-from AnonXMusic import app
-from AnonXMusic.core.userbot import assistants
-from AnonXMusic.misc import SUDOERS, mongodb
-from AnonXMusic.plugins import ALL_MODULES
-from AnonXMusic.utils.database import get_served_chats, get_served_users, get_sudoers
-from AnonXMusic.utils.decorators.language import language, languageCB
-from AnonXMusic.utils.inline.stats import back_stats_buttons, stats_buttons
+from ValentinaMusic import app
+from ValentinaMusic.core.userbot import assistants
+from ValentinaMusic.misc import SUDOERS, mongodb
+from ValentinaMusic.plugins import ALL_MODULES
+from ValentinaMusic.utils.database import get_served_chats, get_served_users, get_sudoers
+from ValentinaMusic.utils.decorators.language import language, languageCB
+from ValentinaMusic.utils.inline.stats import back_stats_buttons, stats_buttons
 from config import BANNED_USERS
 
 
