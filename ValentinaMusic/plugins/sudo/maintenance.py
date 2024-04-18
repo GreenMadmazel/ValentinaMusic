6cf24eda3319a2a859172e60babe3ca0273a608c
ValentineXHashTag <166988254+ValentineXHashTag@users.noreply.github.com>
2024-04-18 16:04:19 +0530

    Update maintenance.py

--- ValentinaMusic/plugins/sudo/maintenance.py
+++ ValentinaMusic/plugins/sudo/maintenance.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.database import (
+from ValentinaMusic import app
+from ValentinaMusic.misc import SUDOERS
+from ValentinaMusic.utils.database import (
     get_lang,
     is_maintenance,
     maintenance_off,
