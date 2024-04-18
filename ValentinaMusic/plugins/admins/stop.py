976502574171d3ac20da2994f92bd051dab1a30d
ValentineXHashTag <166988254+ValentineXHashTag@users.noreply.github.com>
2024-04-18 14:46:42 +0530

    Update stop.py

--- ValentinaMusic/plugins/admins/stop.py
+++ ValentinaMusic/plugins/admins/stop.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.utils.database import set_loop
-from AnonXMusic.utils.decorators import AdminRightsCheck
-from AnonXMusic.utils.inline import close_markup
+from ValentinaMusic import app
+from ValentinaMusic.core.call import Anony
+from ValentinaMusic.utils.database import set_loop
+from ValentinaMusic.utils.decorators import AdminRightsCheck
+from ValentinaMusic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
