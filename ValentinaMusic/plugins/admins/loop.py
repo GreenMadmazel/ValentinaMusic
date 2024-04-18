f9d350e1bb0da91fbe60b701acdf70415743ffaf
ValentineXHashTag <166988254+ValentineXHashTag@users.noreply.github.com>
2024-04-18 14:57:17 +0530

    Update loop.py

--- ValentinaMusic/plugins/admins/loop.py
+++ ValentinaMusic/plugins/admins/loop.py
@@ -1,10 +1,10 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.utils.database import get_loop, set_loop
-from AnonXMusic.utils.decorators import AdminRightsCheck
-from AnonXMusic.utils.inline import close_markup
+from ValentinaMusic import app
+from ValentinaMusic.utils.database import get_loop, set_loop
+from ValentinaMusic.utils.decorators import AdminRightsCheck
+from ValentinaMusic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
